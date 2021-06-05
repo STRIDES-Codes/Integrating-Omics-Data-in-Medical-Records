555d99b5119055526664405a06a79eea85775dc7
abdellahai <aidrissiazami@um6ss.ma>
2021-06-05 11:16:30 +0100

    ''

--- one_folder/frontend/lib/views/login.dart
+++ one_folder/frontend/lib/views/login.dart
@@ -7,17 +7,19 @@ class LoginView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Container(
-          margin: EdgeInsets.symmetric(vertical: MediaQuery.of(context).size.height*0.01, horizontal: MediaQuery.of(context).size.width*0.01),
-          width: MediaQuery.of(context).size.width*0.5,
-          height: MediaQuery.of(context).size.height*0.5,
-          decoration: BoxDecoration(boxShadow: [BoxShadow(color:Colors.grey.withOpacity(0.5),spreadRadius: 5,blurRadius: 7,offset: Offset(0, 3),)],borderRadius: BorderRadius.all(Radius.circular(5))),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            mainAxisSize: MainAxisSize.min,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [],
-            
+        child: Center(
+          child: Container(
+            margin: EdgeInsets.symmetric(vertical: MediaQuery.of(context).size.height*0.01, horizontal: MediaQuery.of(context).size.width*0.01),
+            width: MediaQuery.of(context).size.width*0.5,
+            height: MediaQuery.of(context).size.height*0.5,
+            decoration: BoxDecoration(boxShadow: [BoxShadow(color:Colors.grey.withOpacity(0.5),spreadRadius: 5,blurRadius: 7,offset: Offset(0, 3),)],borderRadius: BorderRadius.all(Radius.circular(5))),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [],
+              
+            ),
           ),
         ),
       ),
