d837fa87d1775184c268eab6df5fa16b50ca31b9
abdellahai <aidrissiazami@um6ss.ma>
2021-06-06 10:54:52 +0100

    add register

--- one_folder/frontend/lib/views/login.dart
+++ one_folder/frontend/lib/views/login.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:frontend/utils/http/post.dart';
 
 class LoginView extends StatefulWidget {
   const LoginView({Key? key}) : super(key: key);
@@ -13,12 +14,14 @@ class _LoginViewState extends State<LoginView> {
   @override
   void initState() {
     super.initState();
-    registerText = 'Register';
+    registerText = 'You don\'t have an account ? Register';
     register = false;
   }
 
   @override
   Widget build(BuildContext context) {
+    final TextEditingController email_ctrl = new TextEditingController();
+    final TextEditingController password_ctrl = new TextEditingController();
     return Scaffold(
       body: SafeArea(
         child: Center(
@@ -26,7 +29,7 @@ class _LoginViewState extends State<LoginView> {
             margin: EdgeInsets.symmetric(
                 vertical: MediaQuery.of(context).size.height * 0.01,
                 horizontal: MediaQuery.of(context).size.width * 0.01),
-            width: MediaQuery.of(context).size.width * 0.5,
+            width: MediaQuery.of(context).size.width * 0.4,
             height: MediaQuery.of(context).size.height * 0.5,
             decoration: BoxDecoration(
                 color: Colors.white,
@@ -40,53 +43,108 @@ class _LoginViewState extends State<LoginView> {
                 ],
                 borderRadius: BorderRadius.all(Radius.circular(5))),
             child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.start,
               mainAxisSize: MainAxisSize.min,
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                Row(
-                  mainAxisSize: MainAxisSize.max,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      "I-Folder",
-                      style:
-                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                    )
-                  ],
+                Padding(
+                  padding: EdgeInsets.only(
+                      top: MediaQuery.of(context).size.height * 0.04),
+                  child: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "I-Folder",
+                        style: TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.bold),
+                      )
+                    ],
+                  ),
                 ),
-                Form(
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.1,
+                ),
+                Center(
+                  child: Form(
+                      child: SizedBox(
+                    width: MediaQuery.of(context).size.width * 0.3,
                     child: Column(
-                  children: [
-                    Visibility(
-                        child: Column(
-                      children: [TextField()],
-                    ))
-                  ],
-                )),
-                Row(
-                  children: [
-                    TextButton(
-                        onPressed: () {
-                          switch (register) {
-                            case false:
-                              setState(() {
-                                register = true;
-                                registerText = "Sign in";
-                              });
-                              break;
-                            case true:
-                              setState(() {
-                                register = false;
-                                registerText = "Register";
-                              });
-                              break;
-                            default:
-                          }
-                        },
-                        child: Text(registerText))
-                  ],
+                      children: [
+                        Visibility(
+                            child: Column(
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: TextField(
+                                controller: email_ctrl,
+                                decoration: InputDecoration(
+                                    hintText: "Email", labelText: "Email"),
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: TextField(
+                                controller: password_ctrl,
+                                obscureText: true,
+                                decoration: InputDecoration(
+                                    hintText: "Password",
+                                    labelText: "Password"),
+                              ),
+                            ),
+                            ElevatedButton(
+                                onPressed: () {
+                                  switch (register) {
+                                    case true:
+                                      Map register_data = {};
+                                      post('register', register_data, '')
+                                          .then((value) {
+                                        ScaffoldMessenger.of(context)
+                                            .showSnackBar(SnackBar(
+                                                content: Text(
+                                                    "Your account has been created, wait for a validation email from our sysadmin")));
+                                      });
+                                      break;
+                                    default:
+                                  }
+                                },
+                                child: Text("Submit"))
+                          ],
+                        ))
+                      ],
+                    ),
+                  )),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(
+                      top: MediaQuery.of(context).size.height * 0.06),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      TextButton(
+                          onPressed: () {
+                            switch (register) {
+                              case false:
+                                setState(() {
+                                  register = true;
+                                  registerText =
+                                      "Have you already an account ? Sign in";
+                                });
+                                break;
+                              case true:
+                                setState(() {
+                                  register = false;
+                                  registerText =
+                                      "Don\'t you have an account ? Register";
+                                });
+                                break;
+                              default:
+                            }
+                          },
+                          child: Text(registerText))
+                    ],
+                  ),
                 ),
               ],
             ),
